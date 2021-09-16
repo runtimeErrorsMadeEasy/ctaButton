62939d3f328d1124557051d964ab72172ab3f0b4
Liz <elizabethblake@psu.edu>
2021-09-16 16:36:57 -0400

    modified style of button

--- cta-button/src/CtaButton.js
+++ cta-button/src/CtaButton.js
@@ -3,34 +3,46 @@ import { html, css, LitElement } from 'lit';
 export class CtaButton extends LitElement {
   static get styles() {
     return css`
-      :host {
-        display: block;
-        padding: 25px;
-        color: var(--cta-button-text-color, #000);
+      .ctaButton {
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        -ms-transform: translate(-50%, -50%);
+        border-radius: 24px;
+        padding: 12px 50px;
+        color: white;
+        background-color: navy;
+        border: 2px solid white;
+        transition-duration: 0.5s;
+        font-family: impact;
+      }
+      .ctaButton:hover {
+        border-radius: 24px;
+        color: navy;
+        background-color: white;
+        border: 2px solid navy;
+        box-shadow: 0 12px 16px 0 rgba(0, 0, 0, 0.24),
+          0 17px 50px 0 rgba(0, 0, 0, 0.19);
       }
     `;
   }
 
-  static get properties() {
-    return {
-      myBool: { type: Boolean },
-    };
-  }
+  // static get properties() {
+  // return {
+  //  };
+  // }
 
-  constructor() {
-    super();
-    this.myBool = false;
-  }
-
-  _disable() {
-    document.getElementById('testButton').onclick = this.myBool;
-  }
+  // constructor() {
+  //  super();
+  // }
 
   render() {
     return html`
-      <button id="testButton" ?disabled="${this.myBool}">test</button>
+      <button class="ctaButton" id="testButton">Penn State Hockey</button>
     `;
   }
 }
 
 // ?disabled="${this._disable}
+// onclick="this.disabled = true;"
